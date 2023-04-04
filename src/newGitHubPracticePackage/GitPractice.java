bfe5ead636f23a6243d9a90cef93f33c83d52b89
91917 <91917@Ashwini>
2023-04-04 12:34:59 +0530

    New GitHub Created and updated

--- src/newGitHubPracticePackage/GitPractice.java
+++ src/newGitHubPracticePackage/GitPractice.java
@@ -3,7 +3,7 @@ package newGitHubPracticePackage;
 public class GitPractice {
 
 	public static void main(String[] args) {
-		System.out.println("New GitHub Created");
+		System.out.println("New GitHub Created and updated");
 
 	}
 
